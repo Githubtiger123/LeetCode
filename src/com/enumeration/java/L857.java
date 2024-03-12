5d34ada6e99ed881bb2fee94cd4156fc7d0d6ae8
Githubtiger123 <93495846+Githubtiger123@users.noreply.github.com>
2024-03-12 20:51:03 +0800

    3.12

--- src/com/enumeration/java/L857.java
+++ src/com/enumeration/java/L857.java
@@ -1,8 +1,11 @@
 package com.enumeration.java;
 
+import org.junit.Test;
+
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 //857. 雇佣 K 名工人的最低成本
 public class L857 {
@@ -14,7 +17,7 @@ public class L857 {
         for (int i = 0; i < n; i++) {
             worker[i][0] = quality[i];
             worker[i][1] = wage[i];
-            worker[i][2] = (double) quality[i] / wage[i];
+            worker[i][2] = (double) wage[i] / quality[i];
         }
         Arrays.sort(worker, new Comparator<double[]>() {
             @Override
@@ -24,10 +27,35 @@ public class L857 {
             }
         });
 
-        double ratio = worker[k - 1][2], ans = 0;
-        for (int i = 0; i < k; i++) {
-            ans += worker[i][0] / ratio;
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> (b - a));
+        int sum = 0, count = 0;
+        double ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (count < k) {
+                queue.offer((int) worker[i][0]);
+                sum += (int) worker[i][0];
+                count++;
+                if (k == count) {
+                    ans = sum * worker[i][2];
+                }
+            } else {
+
+                Integer peek = queue.peek();
+                if (worker[i][0] < peek) {
+                    sum -= queue.poll();
+                    sum += worker[i][0];
+                    queue.offer((int) worker[i][0]);
+                    ans = Math.min(ans, sum * worker[i][2]);
+                }
+            }
         }
+
         return ans;
+    }
+
+    @Test
+    public void test() {
+
+        System.out.println(mincostToHireWorkers(new int[]{3, 1, 10, 10, 1}, new int[]{4, 8, 2, 2, 7}, 3));
     }
 }
