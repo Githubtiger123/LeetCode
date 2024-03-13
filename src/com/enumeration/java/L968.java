5157dd128430b2e8d273962d1213e279eec01286
Githubtiger123 <93495846+Githubtiger123@users.noreply.github.com>
2024-03-13 22:08:07 +0800

    3.13

--- src/com/enumeration/java/L968.java
+++ src/com/enumeration/java/L968.java
@@ -21,52 +21,32 @@ public class L968 {
         }
     }
 
-    private class Info {
+    private static int ans = 0;
 
-        int ans;
-        int status;
+    private int f(TreeNode node) {
 
-        public Info(int ans, int status) {
-            this.ans = ans;
-            this.status = status;
-        }
-    }
-
-    public Info func(TreeNode node) {
         if (node == null) {
-            return new Info(0, 1);
+            return 1;
         }
-
-        Info L = func(node.left);
-        Info R = func(node.right);
-
-        if (L.status == 0 || R.status == 0) {
-
-            return new Info(L.ans + R.ans + 1, 2);
+        int left = f(node.left);
+        int right = f(node.right);
+        if (left == 0 || right == 0) {
+            ans++;
+            return 2;
         }
-
-        if (L.status == 1 && R.status == 1) {
-
-            return new Info(L.ans + R.ans, 0);
+        if (left == 1 && right == 1) {
+            return 0;
         }
-        if (L.status == 2 || R.status == 2) {
-            return new Info(L.ans + R.ans, 1);
-        }
-
-        throw new RuntimeException();
+        return 1;
     }
 
     public int minCameraCover(TreeNode root) {
 
-        if (root != null && root.left == null && root.right == null) {
-
-            return 1;
+        ans = 0;//全局变量共享清零
+        int status = f(root);
+        if (status == 0) {
+            return ans + 1;
         }
-        Info func = func(root);
-        System.out.println(func.status);
-        if (func.status == 0) {
-            return func.ans + 1;
-        }
-        return func.ans;
+        return ans;
     }
 }
